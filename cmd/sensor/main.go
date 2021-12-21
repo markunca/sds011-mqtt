d14b22e98cfe83827b83c1d91311536b4dfd951c
markunca <martin.kuncicky@gmail.com>
2021-12-21 10:28:33 +0100

    Update main.go

--- cmd/sensor/main.go
+++ cmd/sensor/main.go
@@ -4,10 +4,10 @@ import "mattianatali.it/sds011-mqtt/internal/sensor"
 
 func main() {
 	c := sensor.Config{
-		Topic:          "home/serina/edge-rpi/air-quality",
+		Topic:          "dust/PM25",
 		SensorPortPath: "/dev/ttyUSB0",
-		CycleMinutes:   1,
-		MqttBroker:     "tcp://192.168.1.117:1883",
+		CycleMinutes:   5,
+		MqttBroker:     "tcp://192.168.1.60:1883",
 	}
 	sensor.Start(c)
 }
